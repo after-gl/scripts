8bcd5b4b26c1ac2d01c648147198995083e93845
after-gl <afterglowd4@gmail.com>
2024-07-12 08:23:26 +0200

    revert AD

--- anydesk.ps1
+++ anydesk.ps1
@@ -1,5 +1,4 @@
 # Variables
-# Variables
 $downloadUrl = "https://download.anydesk.com/AnyDesk.exe"
 $installerPath = "$env:Temp\AnyDesk.exe"
 $installDir = "C:\Program Files (x86)\AnyDesk"
@@ -14,64 +13,47 @@ function Test-AnyDeskInstalled {
 
 # Function to install AnyDesk
 function Install-AnyDesk {
-    try {
-        # Download the latest AnyDesk version
-        Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath -ErrorAction Stop
+    # Download the latest AnyDesk version
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
 
-        # Install AnyDesk silently
-        $installArgs = "--install `"$installDir`" --start-with-win --create-shortcuts --create-desktop-icon --silent"
-        Start-Process -FilePath $installerPath -ArgumentList $installArgs -Wait
-    } catch {
-        Write-Error "Failed to install AnyDesk: $_"
-        exit 1
-    }
+    # Install AnyDesk silently
+    $installArgs = "--install `"$installDir`" --start-with-win --create-shortcuts --create-desktop-icon --silent"
+    Start-Process -FilePath $installerPath -ArgumentList $installArgs -Wait
 }
 
 # Function to configure unattended access
 function Configure-UnattendedAccess {
-    try {
-        # Set up passworded unattended access
-        Start-Process -FilePath $anydeskPath -ArgumentList "--set-password", $password -Wait -ErrorAction Stop
+    # Set up passworded unattended access
+    # Start-Process -FilePath $anydeskPath -ArgumentList "--set-password", $password -Wait
+    Start-Process -FilePath $anydeskPath -ArgumentList "--set-password", $password -Wait
+    # Start-Process -FilePath $anydeskPath -ArgumentList "--admin-settings:access", $password -Wait
+    
+    # Ensure AnyDesk starts with Windows
+    # Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AnyDesk" -Value "`"$anydeskPath`""
 
-        # Ensure AnyDesk starts with Windows
-        # Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AnyDesk" -Value "`"$anydeskPath`"" -ErrorAction Stop
-
-        # Start AnyDesk
-        # Start-Process -FilePath $anydeskPath -ErrorAction Stop
-    } catch {
-        Write-Error "Failed to configure unattended access: $_"
-        exit 1
-    }
+    # Start AnyDesk
+    # Start-Process -FilePath $anydeskPath
 }
 
 # Function to get AnyDesk ID
 function Get-AnyDeskID {
-    try {
-        $anydeskID = & $anydeskPath --get-id
-        Write-Output "AnyDesk has been installed, configured for unattended access, and set to run with Windows."
-        Write-Output "The AnyDesk ID is: $anydeskID"
-    } catch {
-        Write-Error "Failed to get AnyDesk ID: $_"
-        exit 1
-    }
+    $anydeskID = & $anydeskPath --get-id
+    Write-Output "AnyDesk has been installed, configured for unattended access, and set to run with Windows."
+    Write-Output "The AnyDesk ID is: $anydeskID"
 }
 
 # Main script logic
-try {
-    if (Test-AnyDeskInstalled) {
-        Write-Output "AnyDesk is already installed."
-        Configure-UnattendedAccess
-        Get-AnyDeskID
-    } else {
-        Write-Output "AnyDesk is not installed. Installing now..."
-        Install-AnyDesk
-        Configure-UnattendedAccess
-        Get-AnyDeskID
-    }
-} catch {
-    Write-Error "An unexpected error occurred: $_"
-    exit 1
+if (Test-AnyDeskInstalled) {
+    Write-Output "AnyDesk is already installed."
+    Configure-UnattendedAccess
+    Get-AnydeskID
+    Write-Host "Press any key to continue..."
+    [System.Console]::ReadKey() > $null
+} else {
+    Write-Output "AnyDesk is not installed. Installing now..."
+    Install-AnyDesk
+    Configure-UnattendedAccess
+    Get-AnyDeskID
+    Write-Host "Press any key to continue..."
+    [System.Console]::ReadKey() > $null
 }
-
-Write-Host "Press any key to continue..."
-[System.Console]::ReadKey() > $null
